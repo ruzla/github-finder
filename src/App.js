b621ace909a071263abe935b05407c553d78b46f
Russell <russelldoe@hotmail.co.uk>
2020-12-21 00:23:25 +0000

    refactored classes to functions and refactored state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from '../src/components/layout/Navbar';
-import UserItem from '../src/components/users/UserItem'
+import Users from '../src/components/users/Users'
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const res = await axios
+      .get('https://api.github.com/users');
+
+      this.setState({ users: res.data, loading: false })
+  }
   render() {
     return (
       <div>
-        <nav class="navbar bg-primary">
-          <Navbar />
-        </nav>
-        <UserItem />
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>  
       </div> 
     );
   } 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    }
-
-    static propsTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <div>
+const Navbar = ({ icon, title}) => {
+    return (
+        <div>
+            <nav className="navbar bg-primary">
                 <h1>
-                    <i className={this.props.icon}></i> {this.props.title}
+                    <i className={icon}></i> {title}
                 </h1>
-            </div>
-        )
-    }
+            </nav>     
+        </div>
+    )
+}
+
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propsTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default Navbar
