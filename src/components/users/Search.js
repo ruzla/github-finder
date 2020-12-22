b94e8586a0d98afc26dc310ec97aac0b92e6b88b
Russell <russelldoe@hotmail.co.uk>
2020-12-22 00:07:32 +0000

    added search functionality and clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
     }
 
-    onSubmit = e => {
-        e.preventDefault();
-        console.log(this.state.text);
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
-    onChange = e => this.setState({ text: e.target.value });
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: ''});
+    }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
 
 
     render() {
+        const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -28,6 +37,9 @@ export class Search extends Component {
                         value="Search" 
                         className="btn btn-dark btn-block" />
                 </form>
+                {showClear &&
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+                
             </div>
         )
     }
